1a3e834961835efcabb42c4e09886c9c627ae79e
Phuwadol (Garn) Leangphibul <garnleangphibul@gmail.com>
2025-04-29 12:23:05 +0200

    Add files via upload
    Script for pause menu, need to be implemented still in Unity

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+   public void Resume()
+    {
+        // Turn off Pause Menu UI and change time back to normal
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        // Set the Pause Menu UI as active and freeze time
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting gamne....");
+        Application.Quit();
+    }
+}
